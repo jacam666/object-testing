8510361ffcf84377915de69736b9d0b3b1934f2f
jacam666 <ja6cam@gmail.com>
2021-07-29 20:38:41 +0200

    Add project files.

---
+++ object testing/People/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace object_testing.People
+{
+    public class Person
+    {
+        public string Name;
+        public int age;
+        public bool HasPet;
+
+        public void Greeting()
+        {
+            Console.WriteLine("hi  my name is " + Name + ", my age is " + age + " and has a pet is " + HasPet);
+        }
+    }
+}
---
+++ object testing/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using object_testing.People;
+
+
+namespace object_testing
+{
+
+    class Program
+    {     
+        static void Main(string[] args)
+        {
+            Person person = new();
+
+            person.Name = "ja cam";
+            person.age = 42;
+            person.HasPet = false;
+
+            person.Greeting();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+        //    static void Main(string[] args)
+        //    {
+
+        //        ChestWorkout workOutPlan = new ChestWorkout();
+        //        Exercises workOutExercises = new Exercises();
+        //        workOutPlan.numberOfSets = 20;
+        //        workOutPlan.numberOfRepsPerSets = 10;
+        //        workOutPlan.restBetweenSets = 60;
+        //        workOutExercises.FlatBench = firstExercise;
+
+        //        Console.WriteLine();
+        //    }
+        //}
+        //public class ChestWorkout
+        //{
+        //    public int numberOfSets;
+        //    public int numberOfRepsPerSets;
+        //    public int restBetweenSets;
+        //    public Exercises workoutExercises = new Exercises();
+
+        //}
+        //public class Exercises
+        //{
+        //    public string FlatBench;
+        //    public string InclineBench;
+        //    public string CableFlyes;
+        //    public string InclineDumbbellPress;
+        //    public string PecDec;
+        //}
+
+
+
+
+
+
+
+    }
+
+}
+
+
+
